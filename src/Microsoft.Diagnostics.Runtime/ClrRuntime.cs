aea464bf51a9f98dab93ebf6c137dbeee39f8549
Lee Culver <leculver@microsoft.com>
2017-01-20 15:57:26 -0800

    Update ClrRuntime.cs

--- src/Microsoft.Diagnostics.Runtime/ClrRuntime.cs
+++ src/Microsoft.Diagnostics.Runtime/ClrRuntime.cs
@@ -206,7 +206,8 @@ namespace Microsoft.Diagnostics.Runtime
 
                     if (version.Major == 4 && version.Minor >= 6)
                         return false;
-                } else if (ClrInfo.Flavor == ClrFlavor.CoreCLR)
+                }
+                else if (ClrInfo.Flavor == ClrFlavor.CoreCLR)
                 {
                     return false;
                 }
