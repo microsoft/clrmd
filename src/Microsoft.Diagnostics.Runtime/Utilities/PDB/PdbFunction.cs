e3e050e00a757de451124be173ab266b87e9d18b
Lee Culver <leculver@microsoft.com>
2017-03-14 13:25:49 -0700

    Fix PDB exception
    We need to skip custom metadata altogether.

--- src/Microsoft.Diagnostics.Runtime/Utilities/PDB/PdbFunction.cs
+++ src/Microsoft.Diagnostics.Runtime/Utilities/PDB/PdbFunction.cs
@@ -398,10 +398,6 @@ namespace Microsoft.Diagnostics.Runtime.Utilities.Pdb
                 case 2: break; // this.ReadForwardedToModuleInfo(bits); break;
                 case 3: this.ReadIteratorLocals(bits); break;
                 case 4: this.ReadForwardIterator(bits); break;
-                case 5: break;
-                case 6: break;
-                case 7: break;
-                default: throw new PdbDebugException("Unknown custom metadata item kind: {0}", kind);
             }
             bits.Position = savedPosition + (int)numberOfBytesInItem;
         }
