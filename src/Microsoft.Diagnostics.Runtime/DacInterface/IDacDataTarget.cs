7d289e7afc1f814aec96773bc4b913b01e50a90c
Lee Culver <leculver@microsoft.com>
2018-10-23 17:15:07 -0700

    Fix string marshalling issue

--- src/Microsoft.Diagnostics.Runtime/DacInterface/IDacDataTarget.cs
+++ src/Microsoft.Diagnostics.Runtime/DacInterface/IDacDataTarget.cs
@@ -320,7 +320,7 @@ namespace Microsoft.Diagnostics.Runtime.DacInterface
 
         #region Delegates
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        delegate int GetMetadataDelegate(IntPtr self, string filename, uint imageTimestamp, uint imageSize, IntPtr mvid, uint mdRva, uint flags, uint bufferSize, byte[] buffer, IntPtr dataSize);
+        delegate int GetMetadataDelegate(IntPtr self, [In, MarshalAs(UnmanagedType.LPWStr)] string filename, uint imageTimestamp, uint imageSize, IntPtr mvid, uint mdRva, uint flags, uint bufferSize, byte[] buffer, IntPtr dataSize);
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         delegate int GetMachineTypeDelegate(IntPtr self, out IMAGE_FILE_MACHINE machineType);
