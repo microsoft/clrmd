e5947298ac1eaf1e4db050d153f781f00715934f
Lee Culver <leculver@microsoft.com>
2018-10-24 12:23:15 -0700

    Fix FinalizationQueue test
    The finalizer test wasn't structured in a way to properly collect references when built in the new system.

--- src/TestTargets/FinalizationQueue.cs
+++ src/TestTargets/FinalizationQueue.cs
@@ -4,49 +4,53 @@ using System.Threading;
 
 public class FinalizationQueueTarget
 {
-  public const int ObjectsCountA = 42;
-  public const int ObjectsCountB = 13;
-  
-  private static readonly ICollection<object> _objects = new List<object>();
-  
-  public static void Main(params string[] args)
-  {
-    for (var i = 0; i < ObjectsCountA; i++)
-      _objects.Add(new DieFastA());
+    public const int ObjectsCountA = 42;
+    public const int ObjectsCountB = 13;
+
+    private static readonly ICollection<object> _objects = new List<object>();
+
+    public static void Main(params string[] args)
+    {
+        Test1();
+        GC.Collect();
+        
+        Test2();
+        GC.Collect();
+        GC.Collect();
+
+        throw new Exception();
+    }
     
-    Console.WriteLine(new DieHard());
-    GC.Collect();
+    private static void Test1()
+    {
+        // Pause the finalizer queue
+        Console.WriteLine(new DieHard());
+    }
     
-    for (var i = 0; i < ObjectsCountB; i++)
-      Console.WriteLine(new DieFastB());
-    GC.Collect();
-    
-    throw new Exception();
-  }
+    private static void Test2()
+    {
+        for (var i = 0; i < ObjectsCountA; i++)
+          _objects.Add(new DieFastA());
+
+        for (var i = 0; i < ObjectsCountB; i++)
+          Console.WriteLine(new DieFastB());
+    }
 }
 
 public class DieHard
 {
-  private static readonly WaitHandle _handle = new ManualResetEvent(false);
+    private static readonly WaitHandle _handle = new ManualResetEvent(false);
 
-  ~DieHard()
-  {
-    _handle.WaitOne();
-  }
+    ~DieHard() { _handle.WaitOne(); }
 }
 
 public class DieFastA
 {
-  ~DieFastA()
-  {
-    Console.WriteLine(GetHashCode());
-  }
+  ~DieFastA() { Console.WriteLine("DieFastA: {0}", GetHashCode()); }
 }
 
 public class DieFastB
 {
-  ~DieFastB()
-  {
-    Console.WriteLine(GetHashCode());
-  }
+  ~DieFastB() { Console.WriteLine("DieFastB: {0}", GetHashCode()); }
+  
 }
