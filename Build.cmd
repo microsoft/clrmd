5bacb7e270cfb11c4d7c4d8d3625d2473dacc58a
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-07 15:12:23 -0800

    fix build.cmd casing

--- Build.cmd
+++
@@ -1,8 +0,0 @@
-
-set scriptRoot=%~dp0
-set binDir=%scriptRoot%bin
-
-if not exist %binDir% mkdir %binDir%
-call %scriptRoot%init-tools.cmd
-call %scriptRoot%Tools\dotnetcli\dotnet restore %scriptRoot%src\Microsoft.Diagnostics.Runtime.sln --packages %scriptRoot%packages
-call %scriptRoot%Tools\dotnetcli\dotnet msbuild %scriptRoot%src\Microsoft.Diagnostics.Runtime.sln /p:OutDir="%scriptRoot%\bin" /m /v:m /flp:verbosity=normal;LogFile=%binDir%\msbuild.log %*
