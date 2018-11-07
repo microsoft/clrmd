fb873f8efffbe7258b78f7cb51336421fc92647b
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-11-07 15:06:38 -0800

    Add AzDO build configuration

--- build.cmd
+++ build.cmd
@@ -1,3 +1,8 @@
-@echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -build -restore -pack %*"
-exit /b %ErrorLevel%
+
+set scriptRoot=%~dp0
+set binDir=%scriptRoot%bin
+
+if not exist %binDir% mkdir %binDir%
+call %scriptRoot%init-tools.cmd
+call %scriptRoot%Tools\dotnetcli\dotnet restore %scriptRoot%src\Microsoft.Diagnostics.Runtime.sln --packages %scriptRoot%packages
+call %scriptRoot%Tools\dotnetcli\dotnet msbuild %scriptRoot%src\Microsoft.Diagnostics.Runtime.sln /p:OutDir="%scriptRoot%\bin" /m /v:m /flp:verbosity=normal;LogFile=%binDir%\msbuild.log %*
