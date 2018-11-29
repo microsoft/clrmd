eb4c5b1d97ccfe5488a0293d08d5f2cadf98c384
Ruslan Isakiev <ruslan.isakiev@gmail.com>
2018-11-29 11:48:21 +0300

    switch "Src" to lower case according to the comments

---
+++ src/Microsoft.Diagnostics.Runtime/src/DataReaders/Core/VectorRegisterArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.Diagnostics.Runtime
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct VectorRegisterArea
+    {
+        public const int VectorRegisterSize = 26;
+
+        [FieldOffset(0x0)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = VectorRegisterSize)]
+        public M128A[] VectorRegister;
+
+        [FieldOffset(0x1a0)]
+        public ulong VectorControl;
+
+        public VectorRegisterArea(VectorRegisterArea other) : this()
+        {
+            for (int i = 0; i < VectorRegisterSize; ++i)
+                VectorRegister[i] = other.VectorRegister[i];
+
+            VectorControl = other.VectorControl;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < VectorRegisterSize; ++i)
+                VectorRegister[i].Clear();
+
+            VectorControl = 0;
+        }
+    }
+}
