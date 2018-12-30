9ca063230f5315bb1b32d3c932fe65550c10a069
Nikolay Mitikov <nmi@sitecore.net>
2018-12-30 10:01:54 +0800

    Extract common parts from ClrObject and ClrValueType
    Interface allows treating reference and structs commonly
    for get known type data from known location scenarios

---
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ITypedEntity.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.Diagnostics.Runtime
+{
+    /// <summary>
+    /// Represents an addressable entity (class or struct) with assosiated type.
+    /// </summary>
+    public interface IAddressableTypedEntity: IEquatable<IAddressableTypedEntity>
+    {
+        ulong Address { get; }
+
+        string HexAddress { get; }
+
+        ClrType Type { get; }
+
+        T GetField<T>(string fieldName) where T : struct;
+
+        string GetStringField(string fieldName);
+
+        ClrValueClass GetValueClassField(string fieldName);
+
+        ClrObject GetObjectField(string fieldName);
+    }
+}
