b1b10b97f601972e59ec55c4c2c82133c3715997
Ruslan Isakiev <ruslan.isakiev@gmail.com>
2019-03-29 16:28:36 +0300

    provide xmldocs for exception kinds

--- src/Microsoft.Diagnostics.Runtime/src/Common/ClrDiagnosticsExceptionKind.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ClrDiagnosticsExceptionKind.cs
@@ -12,13 +12,46 @@ namespace Microsoft.Diagnostics.Runtime
     [Serializable]
     public enum ClrDiagnosticsExceptionKind
     {
+        /// <summary>
+        /// Unknown error occured.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// Dump file is corrupted or has an unknown format.
+        /// </summary>
         CorruptedFileOrUnknownFormat,
+
+        /// <summary>
+        /// The caller attempted to re-use an object after calling ClrRuntime.Flush.  See the
+        /// documentation for ClrRuntime.Flush for more details.
+        /// </summary>
         RevisionMismatch,
+
+        /// <summary>
+        /// Something unexpected went wrong with the debugger we used to attach to the process or load the crash dump.
+        /// </summary>
         DebuggerError,
+
+        /// <summary>
+        /// An error occurred while processing the given crash dump.
+        /// </summary>
         CrashDumpError,
+
+        /// <summary>
+        /// Something unexpected went wrong when requesting data from the target process.
+        /// </summary>
         DataRequestError,
+
+        /// <summary>
+        /// Hit an unexpected (non-recoverable) dac error.
+        /// </summary>
         DacError,
+
+        /// <summary>
+        /// The dll of the specified runtime (mscorwks.dll or clr.dll) is loaded into the process, but
+        /// has not actually been initialized and thus cannot be debugged.
+        /// </summary>
         RuntimeUninitialized
     }
 }
