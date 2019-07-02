83ef845bf72f8670cf69e172cc3fb74a16f54aea
Ruslan Isakiev <ruslan.isakiev@gmail.com>
2019-07-02 22:52:50 +0300

    Drop obsolete ModuleInfo.IsRuntime (#255)

--- src/Microsoft.Diagnostics.Runtime/src/DataTargets/DataTargetImpl.cs
+++ src/Microsoft.Diagnostics.Runtime/src/DataTargets/DataTargetImpl.cs
@@ -190,8 +190,6 @@ namespace Microsoft.Diagnostics.Runtime
                     Version = module.Version
                 };
 
-                module.IsRuntime = true; //strange logic here (originally from the ClrInfo constructor)
-
                 versions.Add(new ClrInfo(this, flavor, module, dacInfo, dacLocation));
             }
 
