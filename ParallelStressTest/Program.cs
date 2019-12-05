d035640dcd890e9de0ee748a24601e4636fa6270
Lee Culver <leculver@microsoft.com>
2019-12-05 10:18:06 -0800

    Clean up ParallelStressTest

--- ParallelStressTest/Program.cs
+++ ParallelStressTest/Program.cs
@@ -3,9 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ParallelStressTest
 {
@@ -14,6 +12,7 @@ namespace ParallelStressTest
         const int Iterations = 500;
         const int Threads = 8;
         private static ClrObject[] _expectedObjects;
+        private static ClrSegment[] _segments;
         private static readonly ManualResetEvent _event = new ManualResetEvent(initialState: false);
 
         static void Main(string[] args)
@@ -28,68 +27,72 @@ namespace ParallelStressTest
             using (ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime())
             {
                 _expectedObjects = runtime.Heap.EnumerateObjects().ToArray();
+                _segments = runtime.Heap.Segments.ToArray();
             }
 
-
+            TimeSpan elapsed = default;
             for (int i = 0; i < Iterations; i++)
             {
-                Console.Write($"\rIteration: {i + 1:n0}");
+                Console.Write($"\rIteration: {i + 1:n0} {elapsed}");
+
+                dt.DataReader.FlushCachedData();
                 using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
 
-                List<Task<IReadOnlyList<ClrObject>>> tasks = new List<Task<IReadOnlyList<ClrObject>>>();
+                Thread[] threads = new Thread[Threads];
 
                 for (int j = 0; j < Threads; j++)
-                    tasks.Add(CreateAndStartThread(runtime));
+                    threads[j] = CreateAndStartThread(runtime);
 
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
                 _event.Set();
 
+                foreach (Thread thread in threads)
+                    thread.Join();
 
-                while (tasks.Count > 0)
-                {
-                    int index = Task.WaitAny(tasks.ToArray());
-                    var curr = tasks[index].Result;
-                    tasks.RemoveAt(index);
-
-                    if (_expectedObjects.Length != curr.Count)
-                        throw new InvalidOperationException($"Expected objects enumerated was {_expectedObjects.Length:n0}, got {curr.Count:n0}");
-
-                    for (int j = 0; j < _expectedObjects.Length; j++)
-                    {
-                        if (_expectedObjects[j].Address != curr[j].Address)
-                            throw new InvalidOperationException($"Object {j} was incorrect address: Expected {_expectedObjects[j].Address:x}, got {curr[j].Address:x}");
-
-                        if (_expectedObjects[j].Type != curr[j].Type)
-                            throw new InvalidOperationException($"Object {j} was incorrect type: Expected {_expectedObjects[j].Type.Name}, got {curr[j].Type?.Name ?? ""}");
-                    }
-                }
-
+                sw.Stop();
+                elapsed = sw.Elapsed;
 
                 _event.Reset();
             }
         }
 
-        private static Task<IReadOnlyList<ClrObject>> CreateAndStartThread(ClrRuntime runtime)
+        private static Thread CreateAndStartThread(ClrRuntime runtime)
         {
-            TaskCompletionSource<IReadOnlyList<ClrObject>> source = new TaskCompletionSource<IReadOnlyList<ClrObject>>();
-
-            Thread t = new Thread(() => WorkerThread(runtime, source));
+            Thread t = new Thread(() => WorkerThread(runtime));
             t.Start();
 
-            return source.Task;
+            return t;
         }
 
-        private static void WorkerThread(ClrRuntime runtime, TaskCompletionSource<IReadOnlyList<ClrObject>> source)
+        private static void WorkerThread(ClrRuntime runtime)
         {
             _event.WaitOne();
-            try
+
+            if (_segments.Length != runtime.Heap.Segments.Count)
+                throw new InvalidOperationException($"Segment count mismatch.  Expected {_segments.Length} segments, found {runtime.Heap.Segments.Count}.");
+
+            for (int i = 0; i < _segments.Length; i++)
+                if (runtime.Heap.Segments[i].FirstObject != _segments[i].FirstObject || runtime.Heap.Segments[i].CommittedEnd != _segments[i].CommittedEnd)
+                    throw new InvalidOperationException($"Segment[{i}] range [{runtime.Heap.Segments[i].FirstObject:x12}-{runtime.Heap.Segments[i].CommittedEnd:x12}], expected [{_segments[i].FirstObject:x12}-{_segments[i].CommittedEnd:x12}]");
+
+
+            int count = 0;
+            IEnumerator<ClrObject> enumerator = runtime.Heap.EnumerateObjects().GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                ClrObject[] objects = runtime.Heap.EnumerateObjects().ToArray();
-                source.SetResult(objects);
+                ClrObject curr = enumerator.Current;
+                if (curr.Address != _expectedObjects[count].Address)
+                    throw new InvalidOperationException($"Object {count} was incorrect address: Expected {_expectedObjects[count].Address:x12}, got {curr.Address:x12}");
+
+                if (curr.Type != _expectedObjects[count].Type)
+                    throw new InvalidOperationException($"Object {count} was incorrect type: Expected {_expectedObjects[count].Type.Name}, got {curr.Type?.Name ?? ""}");
+
+                count++;
             }
-            catch (Exception e)
-            {
-                source.SetException(e);
-            }
+
+            if (count != _expectedObjects.Count())
+                throw new InvalidOperationException($"Expected {_expectedObjects.Length:n0} objects, found {count:n0}.");
         }
     }
 }
