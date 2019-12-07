699787fba6b64bb571c2b35bb41389c623128afd
Lee Culver <leculver@microsoft.com>
2019-12-07 11:36:37 -0800

    Split tests

--- src/Microsoft.Diagnostics.Runtime.Tests/src/CacheOptionsTest.cs
+++ src/Microsoft.Diagnostics.Runtime.Tests/src/CacheOptionsTest.cs
@@ -11,69 +11,70 @@ namespace Microsoft.Diagnostics.Runtime.Tests
         [Fact]
         public void MethodCachingTest()
         {
-            {
-                // Test that when we cache method names they are not re-read
-                using DataTarget dt = TestTargets.Types.LoadFullDump();
-                dt.CacheOptions.CacheMethods = true;
+            // Test that when we cache method names they are not re-read
+            using DataTarget dt = TestTargets.Types.LoadFullDump();
+            dt.CacheOptions.CacheMethods = true;
 
-                // We want to make sure we are getting the same string because it was cached,
-                // not because it was interned
-                dt.CacheOptions.CacheMethodNames = StringCaching.Cache;
+            // We want to make sure we are getting the same string because it was cached,
+            // not because it was interned
+            dt.CacheOptions.CacheMethodNames = StringCaching.Cache;
 
-                using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
-                ClrModule module = runtime.GetModule("sharedlibrary.dll");
-                ClrType type = module.GetTypeByName("Foo");
-                ClrMethod method = type.GetMethod("Bar");
-                Assert.NotEqual(0ul, method.MethodDesc);  // Sanity test
+            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
+            ClrModule module = runtime.GetModule("sharedlibrary.dll");
+            ClrType type = module.GetTypeByName("Foo");
+            ClrMethod method = type.GetMethod("Bar");
+            Assert.NotEqual(0ul, method.MethodDesc);  // Sanity test
 
-                ClrMethod method2 = type.GetMethod("Bar");
-                Assert.Equal(method, method2);
-                Assert.Same(method, method2);
-
-
-                string signature1 = method.Signature;
-                string signature2 = method2.Signature;
-                Assert.NotNull(signature1);
-                Assert.Equal(signature1, signature2);
-
-                Assert.Equal(signature1, method.Signature);
-                Assert.Same(signature1, method.Signature);
-            }
-
-            {
-                using DataTarget dt = TestTargets.Types.LoadFullDump();
-                dt.CacheOptions.CacheMethods = false;
-                dt.CacheOptions.CacheMethodNames = StringCaching.None;
-
-                using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
+            ClrMethod method2 = type.GetMethod("Bar");
+            Assert.Equal(method, method2);
+            Assert.Same(method, method2);
 
 
-                ClrModule module = runtime.GetModule("sharedlibrary.dll");
-                ClrType type = module.GetTypeByName("Foo");
-                ClrMethod method = type.GetMethod("Bar");
-                Assert.NotEqual(0ul, method.MethodDesc);  // Sanity test
+            string signature1 = method.Signature;
+            string signature2 = method2.Signature;
+            Assert.NotNull(signature1);
+            Assert.Equal(signature1, signature2);
 
-                ClrMethod method2 = type.GetMethod("Bar");
-                Assert.Equal(method, method2);
-                Assert.NotSame(method, method2);
+            Assert.Equal(signature1, method.Signature);
+            Assert.Same(signature1, method.Signature);
+        }
+
+        [Fact]
+        public void NoMethodCachingTest()
+        {
+            using DataTarget dt = TestTargets.Types.LoadFullDump();
+            dt.CacheOptions.CacheMethods = false;
+            dt.CacheOptions.CacheMethodNames = StringCaching.None;
+
+            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
 
 
-                string signature1 = method.Signature;
-                string signature2 = method2.Signature;
-                Assert.NotNull(signature1);
-                Assert.Equal(signature1, signature2);
+            ClrModule module = runtime.GetModule("sharedlibrary.dll");
+            ClrType type = module.GetTypeByName("Foo");
+            ClrMethod method = type.GetMethod("Bar");
+            Assert.NotEqual(0ul, method.MethodDesc);  // Sanity test
 
-                Assert.Equal(signature1, method.Signature);
-                Assert.NotSame(signature1, method.Signature);
-                Assert.NotSame(method2.Signature, method.Signature);
+            ClrMethod method2 = type.GetMethod("Bar");
+            Assert.Equal(method, method2);
+            Assert.NotSame(method, method2);
 
-                // Ensure that we can swap this at runtime and that we get interned strings
-                dt.CacheOptions.CacheMethodNames = StringCaching.Intern;
 
-                Assert.NotNull(method.Signature);
-                Assert.Same(method2.Signature, method.Signature);
-                Assert.Same(method.Signature, string.Intern(method.Signature));
-            }
+            string signature1 = method.Signature;
+            string signature2 = method2.Signature;
+            Assert.NotNull(signature1);
+            Assert.Equal(signature1, signature2);
+
+            Assert.Equal(signature1, method.Signature);
+            Assert.NotSame(signature1, method.Signature);
+            Assert.NotSame(method2.Signature, method.Signature);
+
+            // Ensure that we can swap this at runtime and that we get interned strings
+            dt.CacheOptions.CacheMethodNames = StringCaching.Intern;
+
+            Assert.NotNull(method.Signature);
+            Assert.Same(method2.Signature, method.Signature);
+            Assert.Same(method.Signature, string.Intern(method.Signature));
+
         }
 
 
@@ -118,8 +119,6 @@ namespace Microsoft.Diagnostics.Runtime.Tests
             Assert.Same(type.Name, type.Name);
             Assert.Same(type.Name, string.Intern(type.Name));
         }
-
-
 
         [Fact]
         public void FieldCachingTest()
