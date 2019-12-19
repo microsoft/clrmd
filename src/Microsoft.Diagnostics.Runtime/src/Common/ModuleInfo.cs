669ea674eaa7cc3b321c4de2c0e16f808062b422
NextTurn <45985406+NextTurn@users.noreply.github.com>
2019-12-20 00:00:00 +0800

    Suppress warning on ModuleInfo

--- src/Microsoft.Diagnostics.Runtime/src/Common/ModuleInfo.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ModuleInfo.cs
@@ -11,7 +11,9 @@ namespace Microsoft.Diagnostics.Runtime
     /// <summary>
     /// Provides information about loaded modules in a DataTarget.
     /// </summary>
+#pragma warning disable CA1001 // Types that own disposable fields should be disposable
     public class ModuleInfo
+#pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
         private PEImage? _image;
         private readonly IDataReader _dataReader;
