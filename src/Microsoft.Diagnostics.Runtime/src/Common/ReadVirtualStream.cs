6f38deb893bf6442dbb6af774bba30135c08543c
Next Turn <45985406+NextTurn@users.noreply.github.com>
2019-12-21 01:40:20 +0800

    Initial works on reimplementing PEImage using S.R.PE.PEReader (#430)
    * Initial works on reimplementing PEImage using S.R.PE.PEReader
    
    * Dispose PEImage
    
    * Suppress warning on ModuleInfo
    
    * Use int.MaxValue when size is unknown

--- src/Microsoft.Diagnostics.Runtime/src/Common/ReadVirtualStream.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ReadVirtualStream.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Diagnostics.Runtime
         {
         }
 
-        public override long Length => throw new NotImplementedException();
+        public override long Length => _len;
 
         public override long Position
         {
