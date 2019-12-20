69de74303bc8f2cb3b2c51519b3ed842d80abd36
Lee Culver <leculver@microsoft.com>
2019-12-20 10:03:24 -0800

    Revert "Initial works on reimplementing PEImage using S.R.PE.PEReader (#430)"
    This reverts commit 6f38deb893bf6442dbb6af774bba30135c08543c.

--- src/Microsoft.Diagnostics.Runtime/src/DacInterface/DacDataTargetWrapper.cs
+++ src/Microsoft.Diagnostics.Runtime/src/DacInterface/DacDataTargetWrapper.cs
@@ -6,7 +6,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.Diagnostics.Runtime.DbgEng;
@@ -38,7 +37,7 @@ namespace Microsoft.Diagnostics.Runtime.DacInterface
             _dataTarget = dataTarget;
             _dataReader = _dataTarget.DataReader;
             _modules = dataTarget.EnumerateModules().ToArray();
-            Array.Sort(_modules, (a, b) => a.ImageBase.CompareTo(b.ImageBase));
+            Array.Sort(_modules, delegate (ModuleInfo a, ModuleInfo b) { return a.ImageBase.CompareTo(b.ImageBase); });
 
             VTableBuilder builder = AddInterface(IID_IDacDataTarget, false);
             builder.AddMethod(new GetMachineTypeDelegate(GetMachineType));
@@ -263,21 +262,21 @@ namespace Microsoft.Diagnostics.Runtime.DacInterface
 
             DebugOnly.Assert(peimage.IsValid);
 
-            int rva = (int)mdRva;
-            int size = (int)bufferSize;
+            uint rva = mdRva;
+            uint size = bufferSize;
             if (rva == 0)
             {
-                DirectoryEntry metadata = peimage.CorHeader.MetadataDirectory;
-                if (metadata.RelativeVirtualAddress == 0)
+                IMAGE_DATA_DIRECTORY metadata = peimage.CorHeader.Metadata;
+                if (metadata.VirtualAddress == 0)
                     return E_FAIL;
 
-                rva = metadata.RelativeVirtualAddress;
-                size = Math.Min(size, metadata.Size);
+                rva = metadata.VirtualAddress;
+                size = Math.Min(bufferSize, metadata.Size);
             }
 
             checked
             {
-                int read = peimage.Read(rva, new Span<byte>(buffer.ToPointer(), size));
+                int read = peimage.Read((int)rva, new Span<byte>(buffer.ToPointer(), (int)size));
                 if (pDataSize != null)
                     *pDataSize = read;
             }
