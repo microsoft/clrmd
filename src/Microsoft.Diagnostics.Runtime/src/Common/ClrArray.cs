756204c2ce9169e0b6d6588222a561dca2f360ab
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-01-05 00:00:00 +0800

    Suppress warning

--- src/Microsoft.Diagnostics.Runtime/src/Common/ClrArray.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ClrArray.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#pragma warning disable CA1721 // Property names should not match get methods
+
 using System;
 
 namespace Microsoft.Diagnostics.Runtime
