c5486b86f600f74b9e729f2a8ea21f6587286afb
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-01-18 00:00:00 +0800

    Remove unused members

--- src/Microsoft.Diagnostics.Runtime/src/Utilities/Platform/WindowsFunctions.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Utilities/Platform/WindowsFunctions.cs
@@ -94,7 +94,7 @@ namespace Microsoft.Diagnostics.Runtime.Utilities
 
         public override IntPtr LoadLibrary(string lpFileName)
         {
-            return NativeMethods.LoadLibraryEx(lpFileName, 0, NativeMethods.LoadLibraryFlags.NoFlags);
+            return NativeMethods.LoadLibrary(lpFileName);
         }
 
         internal static class NativeMethods
@@ -113,31 +113,12 @@ namespace Microsoft.Diagnostics.Runtime.Utilities
             [DllImport(Kernel32LibraryName, SetLastError = true)]
             public static extern unsafe bool EnumProcesses(int[] lpidProcess, int cb, out int lpcbNeeded);
 
-            public const uint FILE_MAP_READ = 4;
-
             [DllImport(Kernel32LibraryName)]
             [return: MarshalAs(UnmanagedType.Bool)]
             public static extern bool FreeLibrary(IntPtr hModule);
 
-            public static IntPtr LoadLibrary(string lpFileName)
-            {
-                return LoadLibraryEx(lpFileName, 0, LoadLibraryFlags.NoFlags);
-            }
-
-            [DllImport(Kernel32LibraryName, CharSet = CharSet.Unicode, SetLastError = true, EntryPoint = "LoadLibraryExW")]
-            public static extern IntPtr LoadLibraryEx(string fileName, int hFile, LoadLibraryFlags dwFlags);
-
-            [Flags]
-            public enum LoadLibraryFlags : uint
-            {
-                NoFlags = 0x00000000,
-                DontResolveDllReferences = 0x00000001,
-                LoadIgnoreCodeAuthzLevel = 0x00000010,
-                LoadLibraryAsDatafile = 0x00000002,
-                LoadLibraryAsDatafileExclusive = 0x00000040,
-                LoadLibraryAsImageResource = 0x00000020,
-                LoadWithAlteredSearchPath = 0x00000008
-            }
+            [DllImport(Kernel32LibraryName, CharSet = CharSet.Unicode, SetLastError = true, EntryPoint = "LoadLibraryW")]
+            public static extern IntPtr LoadLibrary(string lpLibFileName);
 
             [DllImport(Kernel32LibraryName)]
             [return: MarshalAs(UnmanagedType.Bool)]
@@ -151,8 +132,6 @@ namespace Microsoft.Diagnostics.Runtime.Utilities
 
             [DllImport(VersionLibraryName, CharSet = CharSet.Unicode, EntryPoint = "VerQueryValueW")]
             public static extern bool VerQueryValue(byte* pBlock, string pSubBlock, out IntPtr val, out int len);
-
-            public static short IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR = 14;
 
             [DllImport(Kernel32LibraryName)]
             public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
