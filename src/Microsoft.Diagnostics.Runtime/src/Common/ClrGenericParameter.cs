ebe833ec32c942b41cc736401a00c2ec2e9c5e0b
Lee Culver <leculver@microsoft.com>
2020-04-29 12:28:53 -0700

    Mark ClrGenericParameter's constructor as public

--- src/Microsoft.Diagnostics.Runtime/src/Common/ClrGenericParameter.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/ClrGenericParameter.cs
@@ -6,17 +6,35 @@ using System.Reflection;
 
 namespace Microsoft.Diagnostics.Runtime
 {
+    /// <summary>
+    /// Returns information about generic parameters.
+    /// </summary>
     public readonly struct ClrGenericParameter
     {
+        /// <summary>
+        /// The metadata token of the parameter.
+        /// </summary>
         public int MetadataToken { get; }
 
+        /// <summary>
+        /// The index of the parameter.
+        /// </summary>
         public int Index { get; }
 
+        /// <summary>
+        /// The attributes of the parameter.
+        /// </summary>
         public GenericParameterAttributes Attributes { get; }
 
+        /// <summary>
+        /// The name of the parameter.
+        /// </summary>
         public string Name { get; }
 
-        internal ClrGenericParameter(int metadataToken, int index, GenericParameterAttributes attributes, string name)
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ClrGenericParameter(int metadataToken, int index, GenericParameterAttributes attributes, string name)
         {
             MetadataToken = metadataToken;
             Index = index;
