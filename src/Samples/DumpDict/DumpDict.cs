371981ffac0d9abec2951639d5ed8029765a96b5
Lee Culver <leculver@microsoft.com>
2020-06-03 13:44:37 -0700

    Add DumpDict and DumpHeap samples

---
+++ src/Samples/DumpDict/DumpDict.cs
@@ -0,0 +1,64 @@
+﻿// Please go to the ClrMD project page on github for full source and to report issues:
+//    https://github.com/Microsoft/clrmd
+
+using System;
+using Microsoft.Diagnostics.Runtime;
+using System.IO;
+using System.Linq;
+
+static class Program
+{
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Usage: DumpDict [dump] [objref]");
+            Environment.Exit(1);
+        }
+
+        if (ulong.TryParse(args[2], System.Globalization.NumberStyles.HexNumber, null, out ulong objAddr))
+        {
+            Console.WriteLine($"Could not parse object ref '{args[2]}'.");
+            Environment.Exit(1);
+        }
+
+        string dumpFileName = args[0];
+        string dacPath = args[1];
+
+        using DataTarget dataTarget = DataTarget.LoadDump(dumpFileName);
+        using ClrRuntime runtime = dataTarget.ClrVersions.Single().CreateRuntime();
+        ClrHeap heap = runtime.Heap;
+
+        ClrObject obj = heap.GetObject(objAddr);
+
+        if (!obj.IsValidObject)
+        {
+            Console.WriteLine("Invalid object {0:X}", obj);
+            return;
+        }
+
+        if (!obj.Type.Name.StartsWith("System.Collections.Generic.Dictionary"))
+        {
+            Console.WriteLine("Error: Expected object {0:X} to be a dictionary, instead it's of type '{1}'.");
+            return;
+        }
+
+        // Get the entries field.
+        ClrArray entries = obj.ReadObjectField("entries").AsArray();
+
+        Console.WriteLine("{0,8} {1,16} : {2}", "hash", "key", "value");
+        for (int i = 0; i < entries.Length; ++i)
+        {
+            ClrObject entry = entries.GetObjectValue(i);
+
+            // TODO: Need to handle non-object references
+            int hashCode = entry.ReadField<int>("hashCode");
+            ClrObject key = entry.ReadObjectField("key");
+            ClrObject value = entry.ReadObjectField("value");
+
+            Console.WriteLine($"{hashCode:x} {key} -> {value}");
+        }
+    }
+}
+
---
+++ src/Samples/DumpHeap/DumpHeap.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Diagnostics.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine($"Usage: dumpheap.exe [crashdump]");
+            Environment.Exit(1);
+        }
+
+        Dictionary<ulong, (int Count, ulong Size, string Name)> stats = new Dictionary<ulong, (int Count, ulong Size, string Name)>();
+
+        using DataTarget dataTarget = DataTarget.LoadDump(args[0]);
+        foreach (ClrInfo clr in dataTarget.ClrVersions)
+        {
+            using ClrRuntime runtime = clr.CreateRuntime();
+            ClrHeap heap = runtime.Heap;
+
+            Console.WriteLine("{0,12} {0,12} {1,8} {2}", "Object", "MethodTable", "Size", "Type");
+            foreach (ClrObject obj in heap.EnumerateObjects())
+            {
+                Console.WriteLine($"{obj.Address:x12} {obj.Type.MethodTable:x12} {obj.Size:x8} {obj.Type.Name}");
+
+                if (stats.TryGetValue(obj.Type.MethodTable, out (int Count, ulong Size, string Name) item))
+                    item = (0, 0, obj.Type.Name);
+
+                stats[obj.Type.MethodTable] = (item.Count + 1, item.Size + obj.Size, item.Name);
+            }
+
+            Console.WriteLine("Stats:");
+
+            var sorted = from i in stats
+                         orderby i.Value.Size ascending
+                         select new
+                         {
+                             i.Value.Name,
+                             i.Value.Size,
+                             i.Value.Count
+                         };
+
+            foreach (var item in sorted)
+                Console.WriteLine($"{item.Size:x12} {item.Count:n0}\t{item.Name}");
+        }
+    }
+}
