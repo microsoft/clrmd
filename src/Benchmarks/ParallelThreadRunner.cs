e3b017e9fd4059e5c9861894f4bb7cd4ff34bc85
Lee Culver <leculver@microsoft.com>
2020-06-16 11:35:35 -0700

    Add parallel benchmarks

---
+++ src/Benchmarks/ParallelThreadRunner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Benchmarks
+{
+    internal sealed class ThreadParallelRunner<T>
+    {
+        private int _started;
+        private Action<T> _action;
+        private Thread[] _threads;
+        private ManualResetEvent _evt = new ManualResetEvent(false);
+        private volatile int _count;
+        private List<T>[] _data;
+
+        public ThreadParallelRunner(int count, IEnumerable<T> items)
+        {
+            if (count <= 0)
+                throw new ArgumentException($"{nameof(count)} must be greater than 0", nameof(count));
+
+            _count = count;
+
+            _data = new List<T>[_count];
+            for (int i = 0; i < _count; i++)
+                _data[i] = new List<T>();
+
+            int j = 0;
+            foreach (T item in items)
+            {
+                _data[j++].Add(item);
+                j %= _count;
+            }
+        }
+
+        public void Setup()
+        {
+            _started = 0;
+            _evt.Reset();
+            _threads = new Thread[_count];
+
+            for (int i = 0; i < _count; i++)
+            {
+                _threads[i] = new Thread(ThreadProc) { Name = $"Action runner i" };
+                _threads[i].Start(_data[i]);
+            }
+
+            while (_count != _started)
+                Thread.Sleep(100);
+        }
+
+        public void Run(Action<T> action)
+        {
+            _action = action;
+
+            if (!_threads[0].IsAlive)
+                throw new InvalidOperationException();
+
+            _evt.Set();
+            foreach (Thread thread in _threads)
+                thread.Join();
+        }
+
+        private void ThreadProc(object o)
+        {
+            Interlocked.Increment(ref _started);
+            _evt.WaitOne();
+
+            List<T> items = (List<T>)o;
+            foreach (T item in items)
+                _action(item);
+        }
+    }
+}
---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.CsProj;
+using BenchmarkDotNet.Toolchains.DotNetCli;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Benchmarks
+{
+    internal static class Program
+    {
+        public static string CrashDump { get; private set; }
+        public static bool ShouldTestOSMemoryFeatures
+        {
+            get
+            {
+                // Currently the only time the CacheOptions.UseOSMemoryFeatures flag makes a behavioral change is
+                // on Windows and when AWE is enabled.  We will skip that part of the test if the user isn't running
+                // in that environment so that the tests don't run as long and so that the results aren't confused
+                // as having tested the AWE reader when we didn't.
+                return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && AweHelpers.IsAweEnabled;
+            }
+        }
+
+        static void Main(string[] _)
+        {
+            CrashDump = Environment.GetEnvironmentVariable("ClrmdBenchmarkDumpFile");
+            if (string.IsNullOrWhiteSpace(CrashDump))
+            {
+                Console.WriteLine($"You must set the 'ClrmdBenchmarkDumpFile' environment variable before running this program.");
+                Environment.Exit(1);
+            }
+
+            var settings = NetCoreAppSettings.NetCoreApp31.WithCustomDotNetCliPath(@"C:\Program Files (x86)\dotnet\dotnet.exe");
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            Job job = Job.RyuJitX86.With(CsProjCoreToolchain.From(settings))
+                        .WithId("32bit")
+                        .WithWarmupCount(1) // 1 warmup is enough for our purpose
+                        .WithIterationTime(TimeInterval.FromSeconds(25)) // the default is 0.5s per iteration, which is slighlty too much for us
+                        .WithMinIterationCount(10)
+                        .WithMaxIterationCount(20) // we don't want to run more that 20 iterations
+                        .DontEnforcePowerPlan(); // make sure BDN does not try to enforce High Performance power plan on Windows;
+
+            config.Add(job);
+            BenchmarkRunner.Run<ParallelHeapBenchmarks>(config);
+        }
+    }
+}
