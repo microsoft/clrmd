microsoft/clrmd

commit ad471fab27a9bd6ef27f68e2a764dca9a3d830c6

IMemorReader.Read should return bytes read (#678)

src/Microsoft.Diagnostics.Runtime/src/Common/MemoryReader.cs
@@ -139,7 +139,7 @@ namespace Microsoft.Diagnostics.Runtime
         private bool MisalignedRead(ulong addr, out ulong value)
         {
             Span<byte> span = stackalloc byte[IntPtr.Size];
-            bool res = _dataReader.Read(addr, span, out int size);
+            bool res = _dataReader.Read(addr, span) == IntPtr.Size;
 
             value = span.AsPointer();
 
@@ -149,7 +149,7 @@ namespace Microsoft.Diagnostics.Runtime
         private bool MisalignedRead(ulong addr, out uint value)
         {
             Span<byte> span = stackalloc byte[sizeof(uint)];
-            bool res = _dataReader.Read(addr, span, out _);
+            bool res = _dataReader.Read(addr, span) == sizeof(uint);
 
             value = span.AsUInt32();
 
@@ -159,7 +159,7 @@ namespace Microsoft.Diagnostics.Runtime
         private bool MisalignedRead(ulong addr, out int value)
         {
             Span<byte> span = stackalloc byte[sizeof(int)];
-            bool res = _dataReader.Read(addr, span, out _);
+            bool res = _dataReader.Read(addr, span) == sizeof(int);
 
             value = span.AsInt32();
 
@@ -168,16 +168,16 @@ namespace Microsoft.Diagnostics.Runtime
 
         private bool MoveToPage(ulong addr)
         {
-            _currPageStart = addr;
-            bool res = _dataReader.Read(_currPageStart, _data, out _currPageSize);
+            _currPageSize = _dataReader.Read(_currPageStart, _data);
 
-            if (!res)
+            if (_currPageSize == 0)
             {
                 _currPageStart = 0;
-                _currPageSize = 0;
+                return false;
             }
 
-            return res;
+            _currPageStart = addr;
+            return true;
         }
     }
 }

src/Microsoft.Diagnostics.Runtime.Tests/src/MemoryReaderTests.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Diagnostics.Runtime.Tests
                             continue;
 
                         Span<byte> slice = buffer.Slice(0, sizeof(ulong) + i);
-                        if (!dt.DataReader.Read(expectedOffset, slice, out int read) || read != slice.Length)
+                        if (dt.DataReader.Read(expectedOffset, slice) != slice.Length)
                             continue;
 
                         ulong addressFound = dt.DataReader.SearchMemory(segment.Start, (int)segment.Length, buffer.Slice(0, i + sizeof(ulong)));

src/Microsoft.Diagnostics.Runtime/src/Builders/StringReader.cs
@@ -41,10 +41,11 @@ namespace Microsoft.Diagnostics.Runtime.Builders
             {
                 Span<char> charSpan = new Span<char>(buffer).Slice(0, length);
                 Span<byte> bytes = MemoryMarshal.AsBytes(charSpan);
-                if (!reader.Read(data, bytes, out int count))
+                int read = reader.Read(data, bytes);
+                if (read == 0)
                     return null;
 
-                return Encoding.Unicode.GetString(bytes.Slice(0, count));
+                return Encoding.Unicode.GetString(bytes.Slice(0, read));
             }
             finally
             {

src/Microsoft.Diagnostics.Runtime/src/Common/ReadVirtualStream.cs
@@ -44,11 +44,10 @@ namespace Microsoft.Diagnostics.Runtime
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            if (_dataReader.Read((ulong)(_pos + _disp), new Span<byte>(buffer, offset, count), out int read))
+            int read = _dataReader.Read((ulong)(_pos + _disp), new Span<byte>(buffer, offset, count));
+            if (read > 0)
             {
-                if (read > 0)
-                    _pos += read;
-
+                _pos += read;
                 return read;
             }
 

src/Microsoft.Diagnostics.Runtime/src/DacInterface/DacDataTargetWrapper.cs
@@ -133,7 +133,8 @@ namespace Microsoft.Diagnostics.Runtime.DacInterface
                 return HResult.E_FAIL;
             }
 
-            if (_dataReader.Read(address, span, out int read))
+            int read = _dataReader.Read(address, span);
+            if (read > 0)
             {
                 bytesRead = read;
                 return HResult.S_OK;

src/Microsoft.Diagnostics.Runtime/src/DataReaders/Core/CoreDumpReader.cs
@@ -154,17 +154,10 @@ namespace Microsoft.Diagnostics.Runtime
             return _core.LoadedImages.First(image => (ulong)image.BaseAddress == baseAddress).Open();
         }
 
-        public bool Read(ulong address, Span<byte> buffer, out int bytesRead)
+        public int Read(ulong address, Span<byte> buffer)
         {
             DebugOnly.Assert(!buffer.IsEmpty);
-            if (address > long.MaxValue)
-            {
-                bytesRead = 0;
-                return false;
-            }
-
-            bytesRead = _core.ReadMemory((long)address, buffer);
-            return bytesRead > 0;
+            return address > long.MaxValue ? 0 : _core.ReadMemory((long)address, buffer);
         }
 
         public ulong ReadPointer(ulong address)
@@ -176,7 +169,7 @@ namespace Microsoft.Diagnostics.Runtime
         public unsafe bool Read<T>(ulong address, out T value) where T : unmanaged
         {
             Span<byte> buffer = stackalloc byte[sizeof(T)];
-            if (Read(address, buffer, out int size) && size == sizeof(T))
+            if (Read(address, buffer) == sizeof(T))
             {
                 value = Unsafe.As<byte, T>(ref MemoryMarshal.GetReference(buffer));
                 return true;
@@ -195,7 +188,7 @@ namespace Microsoft.Diagnostics.Runtime
         public bool ReadPointer(ulong address, out ulong value)
         {
             Span<byte> buffer = stackalloc byte[IntPtr.Size];
-            if (Read(address, buffer, out int size) && size == IntPtr.Size)
+            if (Read(address, buffer) == IntPtr.Size)
             {
                 value = buffer.AsPointer();
                 return true;

src/Microsoft.Diagnostics.Runtime/src/DataReaders/DbgEng/DbgEngDataReader.cs
@@ -255,11 +255,10 @@ namespace Microsoft.Diagnostics.Runtime
             Interlocked.Increment(ref s_totalInstanceCount);
         }
 
-        public bool Read(ulong address, Span<byte> buffer, out int read)
+        public int Read(ulong address, Span<byte> buffer)
         {
             DebugOnly.Assert(!buffer.IsEmpty);
-            read = _spaces.ReadVirtual(address, buffer);
-            return read > 0;
+            return _spaces.ReadVirtual(address, buffer);
         }
 
         public ulong ReadPointer(ulong address)
@@ -271,7 +270,7 @@ namespace Microsoft.Diagnostics.Runtime
         public unsafe bool Read<T>(ulong address, out T value) where T : unmanaged
         {
             Span<byte> buffer = stackalloc byte[sizeof(T)];
-            if (Read(address, buffer, out int size) && size == sizeof(T))
+            if (Read(address, buffer) == sizeof(T))
             {
                 value = Unsafe.As<byte, T>(ref MemoryMarshal.GetReference(buffer));
                 return true;
@@ -290,7 +289,7 @@ namespace Microsoft.Diagnostics.Runtime
         public bool ReadPointer(ulong address, out ulong value)
         {
             Span<byte> buffer = stackalloc byte[IntPtr.Size];
-            if (Read(address, buffer, out int size) && size == IntPtr.Size)
+            if (Read(address, buffer) == IntPtr.Size)
             {
                 value = buffer.AsPointer();
                 return true;

src/Microsoft.Diagnostics.Runtime/src/DataReaders/IMemoryReader.cs
@@ -19,9 +19,8 @@ namespace Microsoft.Diagnostics.Runtime
         /// </summary>
         /// <param name="address">The address of memory to read.</param>
         /// <param name="buffer">The buffer to write to.</param>
-        /// <param name="bytesRead">The number of bytes actually read out of the target process.</param>
-        /// <returns>True if any bytes were read at all, false if the read failed (and no bytes were read).</returns>
-        bool Read(ulong address, Span<byte> buffer, out int bytesRead);
+        /// <returns>The number of bytes read into the buffer.</returns>
+        int Read(ulong address, Span<byte> buffer);
 
         /// <summary>
         /// Read an unmanaged value from the given address.

commit 4f052d467972806cce2f1529f63651346e03ac2f

Fix heap walking regression (#773)
* Fix cache regression

Fix regression introduced by #678.  We passed the wrong variable into _dataReader.Read, resulting in a failed read (and bad heap enumeration perf).

* Remove test that was causing failures

We can't hard code method tables and object counts.  Should have caught that on code review.

* Fix index out of bound exception

src/Microsoft.Diagnostics.Runtime.Utilities/ClrHeapExtensions.cs
@@ -21,6 +21,5 @@ namespace Microsoft.Diagnostics.Runtime.Utilities
                 .Where(t => t.totalSize > minTotalSize)
                 .OrderBy(t => t.totalSize);
         }
-
     }
 }

src/Microsoft.Diagnostics.Runtime/src/Common/MemoryReader.cs
@@ -168,7 +168,7 @@ namespace Microsoft.Diagnostics.Runtime
 
         private bool MoveToPage(ulong addr)
         {
-            _currPageSize = _dataReader.Read(_currPageStart, _data);
+            _currPageSize = _dataReader.Read(addr, _data);
 
             if (_currPageSize == 0)
             {

src/Microsoft.Diagnostics.Runtime/src/Windows/CachedMemoryReader.cs
@@ -192,11 +192,16 @@ namespace Microsoft.Diagnostics.Runtime.Windows
 
                 if (address != targetSegment.VirtualAddress)
                 {
+                    int prevSegmentIndex = curSegmentIndex;
+
                     curSegmentIndex = segments.Search(address, (x, addr) => (x.VirtualAddress <= addr && addr < x.VirtualAddress + x.Size) ? 0 : x.VirtualAddress.CompareTo(addr));
                     if (curSegmentIndex == -1)
                     {
-                        _lastAccessed.SegmentIndex = curSegmentIndex - 1;
-                        _lastAccessed.Segment = segments[_lastAccessed.SegmentIndex];
+                        if (prevSegmentIndex >= 0)
+                        {
+                            _lastAccessed.SegmentIndex = prevSegmentIndex;
+                            _lastAccessed.Segment = segments[_lastAccessed.SegmentIndex];
+                        }
 
                         return totalBytes;
                     }

src/Microsoft.Diagnostics.Runtime.Tests/src/HeapTests.cs
@@ -263,28 +263,5 @@ namespace Microsoft.Diagnostics.Runtime.Tests
             using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
             return runtime.Heap.EnumerateObjects().ToArray();
         }
-
-        [Fact]
-        public void DumpHeapStat()
-        {
-            using DataTarget dt = TestTargets.Types.LoadFullDump();
-            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
-            ClrHeap heap = runtime.Heap;
-            var results = heap.DumpHeapStat(0);
-            var all = results.ToList();
-
-            Assert.Equal(167, all.Count);
-            Assert.Equal<(ulong, int, long)>((0x00007ff9fd29e768, 1, 24), GetRowData("System.Resources.FastResourceComparer"));
-            Assert.Equal<(ulong, int, long)>((0x00007ff9fd295e70, 24, 36152), GetRowData("System.Object[]"));
-            Assert.Equal<(ulong, int, long)>((0x00007ff9fd2959c0, 301, 22700), GetRowData("System.String"));
-
-            (ulong methodTable, int instances, long totalSize) GetRowData(string typeName)
-            {
-                var objectArray = all.Where(a => a.type.Name.Equals(typeName)).Single();
-                var size = objectArray.size;
-                var mt = objectArray.type.MethodTable;
-                return (mt, objectArray.objects.Count(), objectArray.size);
-            }
-        }
     }
 }