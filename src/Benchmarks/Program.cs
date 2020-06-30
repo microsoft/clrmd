aa06441eb132446b357d04a2ef9223e9769749c4
Adam Sitnik <adam.sitnik@gmail.com>
2020-06-30 14:37:06 +0200

    update BDN to latest version to be able to pass `--platform` as argument

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -1,10 +1,8 @@
 ﻿using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Horology;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using Microsoft.Diagnostics.Runtime;
+using Perfolizer.Horology;
 using System;
-using System.Runtime.InteropServices;
 
 namespace Benchmarks
 {
@@ -12,34 +10,18 @@ namespace Benchmarks
     {
         private const string DumpFileEnv = "ClrmdBenchmarkDumpFile";
 
-        public static string CrashDump => Environment.GetEnvironmentVariable(DumpFileEnv) ?? throw new InvalidOperationException("You must set the 'ClrmdBenchmarkDumpFile' environment variable before running this program.");
+        public static string CrashDump => Environment.GetEnvironmentVariable(DumpFileEnv) ?? throw new InvalidOperationException($"You must set the '{DumpFileEnv}' environment variable before running this program.");
 
         static void Main(string[] args)
         {
-            // We want to run this even if we don't use the result to make sure we can successfully load 'CrashDump'.
-            int targetPointerSize = GetTargetPointerSize();
+            var job = Job.Default
+                .WithWarmupCount(1)
+                .WithIterationTime(TimeInterval.FromSeconds(1))
+                .WithMinIterationCount(5)
+                .WithMaxIterationCount(10)
+                .AsDefault(); // AsDefault tells BDN ConfigParser that this job should be extended with setting specified via console line args
 
-            ManualConfig benchmarkConfiguration = ManualConfig.Create(DefaultConfig.Instance);
-
-            string id = $"{RuntimeInformation.OSDescription} {RuntimeInformation.FrameworkDescription} {(targetPointerSize == 4 ? "32bit" : "64bit")}";
-            var job = Job.Default.WithId(id)
-                        .WithWarmupCount(1)
-                        .WithIterationTime(TimeInterval.FromSeconds(1))
-                        .WithMinIterationCount(5)
-                        .WithMaxIterationCount(10)
-                        .DontEnforcePowerPlan() // make sure BDN does not try to enforce High Performance power plan on Windows
-                        .AsDefault();
-
-            benchmarkConfiguration.Add(job);
-
-
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, benchmarkConfiguration);
-        }
-
-        private static int GetTargetPointerSize()
-        {
-            using DataTarget dataTarget = DataTarget.LoadDump(CrashDump, new CacheOptions() { UseOSMemoryFeatures = false });
-            return dataTarget.DataReader.PointerSize;
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance.AddJob(job));
         }
     }
 }
