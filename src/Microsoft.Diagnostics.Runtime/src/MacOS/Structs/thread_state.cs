80212c53b64d904334b9bd9aa92e8b06e0129497
Mike McLaughlin <mikem@microsoft.com>
2021-10-19 18:05:50 -0700

    MacOS dump thread support
    Return ImageSize as IndexFileSize on ModuleInfo for MacOS dumps
    
    Finish IThreadReader on MacOS core reader and add support M1/arm64 dumps
    
    Add IExportReader to MacOS core reader

---
+++ src/Microsoft.Diagnostics.Runtime/src/MacOS/Structs/thread_state.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Diagnostics.Runtime.Utilities
+{
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct thread_state_t
+    {
+        [FieldOffset(0)]
+        public x86_thread_state64_t x64;
+
+        [FieldOffset(0)]
+        public arm_thread_state64_t arm;
+    }
+}
