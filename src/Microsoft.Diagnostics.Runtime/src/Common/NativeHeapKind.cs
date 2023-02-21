a9c2d26621d35579bf0d8b2bd9042b32e286b092
Lee Culver <leculver@microsoft.com>
2023-02-21 09:43:43 -0800

    Add missing PublicAPI entries

--- src/Microsoft.Diagnostics.Runtime/src/Common/NativeHeapKind.cs
+++ src/Microsoft.Diagnostics.Runtime/src/Common/NativeHeapKind.cs
@@ -80,6 +80,10 @@
         /// NewStubPrecodeHeap
         /// </summary>
         NewStubPrecodeHeap,
+
+        /// <summary>
+        /// ThunkHeap
+        /// </summary>
         ThunkHeap,
     }
 }
