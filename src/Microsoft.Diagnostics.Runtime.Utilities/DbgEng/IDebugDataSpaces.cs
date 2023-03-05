ba62117c119b2e687ab1bcb8d010f3dcd1560f8b
Lee Culver <leculver@microsoft.com>
2023-03-05 09:45:43 -0800

    Add WriteVirtual

--- src/Microsoft.Diagnostics.Runtime.Utilities/DbgEng/IDebugDataSpaces.cs
+++ src/Microsoft.Diagnostics.Runtime.Utilities/DbgEng/IDebugDataSpaces.cs
@@ -3,6 +3,7 @@
     public interface IDebugDataSpaces
     {
         bool ReadVirtual(ulong address, Span<byte> buffer, out int read);
+        int WriteVirtual(ulong address, Span<byte> buffer, out int written);
         bool Search(ulong offset, ulong length, Span<byte> pattern, int granularity, out ulong offsetFound);
     }
 }
