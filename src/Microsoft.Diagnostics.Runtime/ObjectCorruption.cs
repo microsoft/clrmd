7f73d0232286081ea34276f62c4c67e6ca5bd68d
Lee Culver <leculver@microsoft.com>
2023-03-09 11:48:31 -0800

    Change MT corruption message
    Differentiate between failing to read a MT, and MT corruption.

--- src/Microsoft.Diagnostics.Runtime/ObjectCorruption.cs
+++ src/Microsoft.Diagnostics.Runtime/ObjectCorruption.cs
@@ -56,6 +56,7 @@ namespace Microsoft.Diagnostics.Runtime
     {
         None,
         ObjectNotOnTheHeap,
+        CouldNotReadMethodTable,
         BadMethodTable,
         BadObjectReference,
         ObjectTooLarge,
