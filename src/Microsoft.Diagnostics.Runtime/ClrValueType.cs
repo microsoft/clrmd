e7e4f72f5d98203436f7997cd55668b1e42bbd31
Juan Sebastian Hoyos Ayala <juan.hoyos@microsoft.com>
2023-03-10 18:14:06 -0800

    Fix next round of warnings

--- src/Microsoft.Diagnostics.Runtime/ClrValueType.cs
+++ src/Microsoft.Diagnostics.Runtime/ClrValueType.cs
@@ -1,11 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.Diagnostics.Runtime.Implementation;
-using Microsoft.Diagnostics.Runtime.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.Diagnostics.Runtime.Implementation;
+using Microsoft.Diagnostics.Runtime.Interfaces;
 
 namespace Microsoft.Diagnostics.Runtime
 {
