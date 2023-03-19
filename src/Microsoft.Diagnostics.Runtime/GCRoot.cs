1420b5b14e1e72106ce674b665565b247910a801
Lee Culver <leculver@microsoft.com>
2023-03-19 14:27:46 -0700

    Find gcroot when predicate is directly at root

--- src/Microsoft.Diagnostics.Runtime/GCRoot.cs
+++ src/Microsoft.Diagnostics.Runtime/GCRoot.cs
@@ -88,6 +88,17 @@ namespace Microsoft.Diagnostics.Runtime
             if (_found.TryGetValue(start, out ChainLink? link))
                 return link;
 
+            if (_targetPredicate is not null && _targetPredicate(start))
+            {
+                link = new ChainLink()
+                {
+                    Object = start,
+                };
+
+                _found.Add(start, link);
+                return link;
+            }
+
             if (start.Type is null || !start.Type.ContainsPointers)
                 return null;
 
