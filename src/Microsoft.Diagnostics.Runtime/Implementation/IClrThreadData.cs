2584b9bbd8a73a9d540211bd2b3d7e69258377b0
Lee Culver <leculver@microsoft.com>
2023-09-18 09:01:41 -0700

    Documentation

--- src/Microsoft.Diagnostics.Runtime/Implementation/IClrThreadData.cs
+++ src/Microsoft.Diagnostics.Runtime/Implementation/IClrThreadData.cs
@@ -145,23 +145,68 @@ namespace Microsoft.Diagnostics.Runtime.Implementation
     }
 
     /// <summary>
-    /// Information about a single root on the stack.
+    /// Information about a single root on the stack.  For this to work properly, we need
+    /// these things to be true:
+    ///     OS Stack Frames - IP and SP should match what the OS stack unwinder says where this
+    ///                       stack frame is.
+    ///     coreclr!Frames  - The StackPointer or the InternalFrame must match the direct
+    ///                       pointer to the clr!Frame object
     /// </summary>
     internal struct StackRootInfo
     {
+        /// <summary>
+        /// The IP associated with this root.  This may be 0 if the root comes from a
+        /// clr!Frame.
+        /// </summary>
         public ulong InstructionPointer { get; set; }
+
+        /// <summary>
+        /// The StackPointer associated with this root.  This may be the SP of the actual
+        /// OS frame on the stack or it may be the pointer of the data on the stack.
+        /// </summary>
         public ulong StackPointer { get; set; }
 
+        /// <summary>
+        /// The coreclr!Frame this root came from, or 0.
+        /// </summary>
         public ulong InternalFrame { get; set; }
 
+        /// <summary>
+        /// Whether the pointer is "interior" or not.  Interior pointers need not point
+        /// to the beginning of the object, and don't even need to point to the GC heap.
+        /// </summary>
         public bool IsInterior { get; set; }
+
+        /// <summary>
+        /// Whether the pointer is pinned or not.
+        /// </summary>
         public bool IsPinned { get; set; }
 
+        /// <summary>
+        /// The address of the stack slot this root is contained in (maybe 0 if the object is
+        /// enregistered).
+        /// </summary>
         public ulong Address { get; set; }
+
+        /// <summary>
+        /// A pointer to the object itself.  This pointer may not point to the beginning of an
+        /// object, or even to the GC heap itself if IsInterior == true.
+        /// </summary>
         public ulong Object { get; set; }
 
+        /// <summary>
+        /// Whether this root is in the thread's register CONTEXT for the frame.
+        /// </summary>
         public bool IsEnregistered { get; set; }
+
+        /// <summary>
+        /// The register name, if available.
+        /// </summary>
         public string? RegisterName { get; set; }
+
+        /// <summary>
+        /// The offset from the register.
+        /// </summary>
         public int RegisterOffset { get; set; }
     }
 }
