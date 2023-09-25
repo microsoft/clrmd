6d7c5a7288c0e93e5eb56893a6064575ac6e3ea8
Lee Culver <leculver@microsoft.com>
2023-09-25 10:21:06 -0700

    Fix issue with domain native heap enumeration (#1188)

---
+++ src/Microsoft.Diagnostics.Runtime.Tests/src/NativeHeapTests.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.Diagnostics.Runtime.Tests
+{
+    public class NativeHeapTests
+    {
+        [Fact]
+        public void TestHeapKinds()
+        {
+            using DataTarget dt = TestTargets.AppDomains.LoadFullDump();
+            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
+
+            ClrNativeHeapInfo[] heaps = runtime.EnumerateClrNativeHeaps().ToArray();
+            Assert.Contains(NativeHeapKind.LoaderCodeHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.StubHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.HighFrequencyHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.LowFrequencyHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.IndirectionCellHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.LookupHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.ResolveHeap, heaps.Select(r => r.Kind));
+            Assert.Contains(NativeHeapKind.DispatchHeap, heaps.Select(r => r.Kind));
+        }
+
+        [Fact]
+        public void TestNativeHeapLengths()
+        {
+            using DataTarget dt = TestTargets.AppDomains.LoadFullDump();
+            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
+
+            foreach (ClrNativeHeapInfo heap in runtime.EnumerateClrNativeHeaps())
+                Assert.NotEqual(0ul, heap.MemoryRange.Length);
+        }
+    }
+}
