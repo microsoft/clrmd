ebe221529ed4db84015a0a40f9ebb7878ee5239e
Lee Culver <leculver@microsoft.com>
2023-10-02 09:11:34 -0700

    Refactor legacy ThreadPool providers (#1196)

--- src/Microsoft.Diagnostics.Runtime/AbstractDac/IAbstractThreadPoolProvider.cs
+++ src/Microsoft.Diagnostics.Runtime/AbstractDac/IAbstractThreadPoolProvider.cs
@@ -11,7 +11,42 @@ namespace Microsoft.Diagnostics.Runtime.AbstractDac
 {
     internal interface IAbstractThreadPoolProvider
     {
-        bool GetLegacyThreadPoolData(out ThreadPoolData data);
-        bool GetLegacyWorkRequestData(ulong workRequest, out WorkRequestData workRequestData);
+        bool GetLegacyThreadPoolData(out LegacyThreadPoolInfo data);
+        bool GetLegacyWorkRequestData(ulong workRequest, out LegacyWorkRequestInfo workRequestData);
+    }
+
+    internal struct LegacyThreadPoolInfo
+    {
+        public int CpuUtilization { get; set; }
+        public int NumIdleWorkerThreads { get; set; }
+        public int NumWorkingWorkerThreads { get; set; }
+        public int NumRetiredWorkerThreads { get; set; }
+        public int MinLimitTotalWorkerThreads { get; set; }
+        public int MaxLimitTotalWorkerThreads { get; set; }
+
+        public ulong FirstUnmanagedWorkRequest { get; set; }
+
+        public ulong HillClimbingLog { get; set; }
+        public uint HillClimbingLogFirstIndex { get; set; }
+        public uint HillClimbingLogSize { get; set; }
+
+        public int NumTimers { get; set; }
+
+        public int NumCPThreads { get; set; }
+        public int NumFreeCPThreads { get; set; }
+        public int MaxFreeCPThreads { get; set; }
+        public int NumRetiredCPThreads { get; set; }
+        public int MaxLimitTotalCPThreads { get; set; }
+        public int CurrentLimitTotalCPThreads { get; set; }
+        public int MinLimitTotalCPThreads { get; set; }
+
+        public ulong AsyncTimerCallbackCompletionFPtr { get; set; }
+    }
+
+    internal struct LegacyWorkRequestInfo
+    {
+        public ulong Function { get; set; }
+        public ulong Context { get; set; }
+        public ulong NextWorkRequest { get; set; }
     }
 }
