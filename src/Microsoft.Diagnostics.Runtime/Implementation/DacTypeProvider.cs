microsoft/clrmd

commit b64f583396784300c7ded11d22c65053197e958a

Get method's hot and cold region info based on the MethodDesc's native address (#1206)
* Get assembly info based on the MethodDesc's native address

* Fix test

---------

Co-authored-by: Mads Gram <magram@microsoft.com>

src/Microsoft.Diagnostics.Runtime/Implementation/DacTypeProvider.cs
@@ -143,14 +143,15 @@ namespace Microsoft.Diagnostics.Runtime
             for (uint i = 0; i < data.NumMethods; i++)
             {
                 ulong slot = _sos.GetMethodTableSlot(methodTable, i);
-                if (_sos.GetCodeHeaderData(slot, out CodeHeaderData chd) && _sos.GetMethodDescData(chd.MethodDesc, 0, out MethodDescData mdd))
+                if (_sos.GetCodeHeaderData(slot, out CodeHeaderData chd) && _sos.GetMethodDescData(chd.MethodDesc, 0, out MethodDescData mdd)
+                    && mdd.HasNativeCode == 1 && _sos.GetCodeHeaderData(mdd.NativeCodeAddr, out CodeHeaderData chdBasedOnNative))
                 {
-                    HotColdRegions regions = new(mdd.NativeCodeAddr, chd.HotRegionSize, chd.ColdRegionStart, chd.ColdRegionSize);
+                    HotColdRegions regions = new(mdd.NativeCodeAddr, chdBasedOnNative.HotRegionSize, chdBasedOnNative.ColdRegionStart, chdBasedOnNative.ColdRegionSize);
                     yield return new()
                     {
-                        MethodDesc = chd.MethodDesc,
+                        MethodDesc = chdBasedOnNative.MethodDesc,
                         Token = (int)mdd.MDToken,
-                        CompilationType = (MethodCompilationType)chd.JITType,
+                        CompilationType = (MethodCompilationType)chdBasedOnNative.JITType,
                         HotCold = regions,
                     };
                 }

src/Microsoft.Diagnostics.Runtime.Tests/src/MethodTests.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Linq;
 using Xunit;
 
@@ -98,6 +99,21 @@ namespace Microsoft.Diagnostics.Runtime.Tests
             string methodName = genericMethod.Signature;
 
             Assert.Equal(')', methodName.Last());
+        }
+
+        [Fact]
+        public void AssemblySize()
+        {
+            using DataTarget dt = TestTargets.AppDomains.LoadFullDump();
+            using ClrRuntime runtime = dt.ClrVersions.Single().CreateRuntime();
+
+            ClrModule module = runtime.GetModule("sharedlibrary.dll");
+            ClrType type = module.GetTypeByName("Foo");
+
+            ClrMethod[] methods = type.Methods.ToArray();
+            ClrMethod genericMethod = type.GetMethod("GenericBar");
+
+            Assert.NotEqual<uint>(0, genericMethod.HotColdInfo.ColdSize + genericMethod.HotColdInfo.HotSize);
         }
     }
 }

commit 0d97698e85de6158b55fc9d6489e5182345be429

Fix a few issues in locating methods
Fix regression introduced by #1206.

src/Microsoft.Diagnostics.Runtime/Implementation/DacTypeProvider.cs
@@ -137,26 +137,73 @@ namespace Microsoft.Diagnostics.Runtime
 
         public IEnumerable<MethodInfo> EnumerateMethodsForType(ulong methodTable)
         {
-            if (!_sos.GetMethodTableData(methodTable, out MethodTableData data) || data.NumMethods == 0)
+            if (!_sos.GetMethodTableData(methodTable, out MethodTableData data))
                 yield break;
 
             for (uint i = 0; i < data.NumMethods; i++)
             {
                 ulong slot = _sos.GetMethodTableSlot(methodTable, i);
-                if (_sos.GetCodeHeaderData(slot, out CodeHeaderData chd) && _sos.GetMethodDescData(chd.MethodDesc, 0, out MethodDescData mdd)
-                    && mdd.HasNativeCode == 1 && _sos.GetCodeHeaderData(mdd.NativeCodeAddr, out CodeHeaderData chdBasedOnNative))
+                if (_sos.GetCodeHeaderData(slot, out CodeHeaderData chd))
                 {
-                    HotColdRegions regions = new(mdd.NativeCodeAddr, chdBasedOnNative.HotRegionSize, chdBasedOnNative.ColdRegionStart, chdBasedOnNative.ColdRegionSize);
-                    yield return new()
+                    if (_sos.GetMethodDescData(chd.MethodDesc, 0, out MethodDescData mdd))
                     {
-                        MethodDesc = chdBasedOnNative.MethodDesc,
-                        Token = (int)mdd.MDToken,
-                        CompilationType = (MethodCompilationType)chdBasedOnNative.JITType,
-                        HotCold = regions,
-                    };
+                        ulong md = chd.MethodDesc;
+                        uint compilation = chd.JITType;
+
+                        HotColdRegions regions;
+                        if (mdd.HasNativeCode != 0 && _sos.GetCodeHeaderData(mdd.NativeCodeAddr, out CodeHeaderData chdBasedOnNative))
+                        {
+                            regions = new(mdd.NativeCodeAddr, chdBasedOnNative.HotRegionSize, chdBasedOnNative.ColdRegionStart, chdBasedOnNative.ColdRegionSize);
+                            md = chdBasedOnNative.MethodDesc;
+                            compilation = chdBasedOnNative.JITType;
+                        }
+                        else
+                        {
+                            regions = new(mdd.NativeCodeAddr, chd.HotRegionSize, chd.ColdRegionStart, chd.ColdRegionSize);
+                        }
+
+                        yield return new()
+                        {
+                            MethodDesc = md,
+                            Token = (int)mdd.MDToken,
+                            CompilationType = (MethodCompilationType)compilation,
+                            HotCold = regions,
+                        };
+                    }
                 }
             }
         }
+
+        //private bool TryGetNumberMethods(ulong methodTable, out int count)
+        //{
+        //    count = 0;
+        //    while (methodTable != 0)
+        //    {
+        //        if (methodTable == _objectMt)
+        //        {
+        //            if (_systemObjectMethodCount < 0)
+        //            {
+        //                if (!_sos.GetMethodTableData(methodTable, out MethodTableData objData))
+        //                    return false;
+
+        //                _systemObjectMethodCount = objData.NumMethods;
+        //                count += objData.NumMethods;
+        //            }
+        //            else
+        //            {
+        //                count += _systemObjectMethodCount;
+        //            }
+
+        //            break;
+        //        }
+
+
+        //        count += data.NumMethods;
+        //        methodTable = data.ParentMethodTable;
+        //    }
+
+        //    return true;
+        //}
 
         public IEnumerable<FieldInfo> EnumerateFields(TypeInfo type, int baseFieldCount)
         {