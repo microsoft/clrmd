a29125b829ace231dca9450efdbcdd6874c8ea59
William van der Walt <williamvdw@outlook.com>
2024-04-15 19:39:16 +0200

    Made changes requested in CR. Removed u suffix from MINIDUMP_STREAM_TYPE, added braces to IDebugAdvancedWrapper request method, and changed buffer size to a constant.

--- src/Microsoft.Diagnostics.Runtime.Utilities/DbgEng/Wrappers/IDebugAdvancedWrapper.cs
+++ src/Microsoft.Diagnostics.Runtime.Utilities/DbgEng/Wrappers/IDebugAdvancedWrapper.cs
@@ -11,6 +11,7 @@ namespace Microsoft.Diagnostics.Runtime.Utilities.DbgEng
     [DynamicInterfaceCastableImplementation]
     internal unsafe interface IDebugAdvancedWrapper : IDebugAdvanced
     {
+        private const int commentBufferSize = 1048576;
         int IDebugAdvanced.GetThreadContext(Span<byte> buffer)
         {
             GetVTable(this, out nint self, out IDebugAdvancedVtable* vtable);
@@ -23,29 +24,33 @@ namespace Microsoft.Diagnostics.Runtime.Utilities.DbgEng
         {
             GetVTable(this, out nint self, out IDebugAdvancedVtable* vtable);
             comment = null;
-            char[] buffer = ArrayPool<char>.Shared.Rent(1048576/2);
+            char[] buffer = ArrayPool<char>.Shared.Rent(commentBufferSize / 2);
             try
             {
                 fixed (char* ptr = buffer)
                 {
                     DEBUG_READ_USER_MINIDUMP_STREAM minidumpStream = default;
                     minidumpStream.StreamType = MINIDUMP_STREAM_TYPE.CommentStreamW;
-                    minidumpStream.Flags = 0u;
-                    minidumpStream.Offset = 0u;
+                    minidumpStream.Flags = 0;
+                    minidumpStream.Offset = 0;
                     minidumpStream.Buffer = new IntPtr(ptr);
                     minidumpStream.BufferSize = (uint)(buffer.Length * sizeof(char));
-                    minidumpStream.BufferUsed = 0u;
+                    minidumpStream.BufferUsed = 0;
                     HResult hr = vtable->Request(self, (uint)DEBUG_REQUEST.READ_USER_MINIDUMP_STREAM, &minidumpStream, sizeof(DEBUG_READ_USER_MINIDUMP_STREAM), null, 0, null);
+
                     if (hr < 0)
+                    {
                         return hr;
-                    else
-                    if (minidumpStream.BufferUsed > 1)
+                    }
+                    else if (minidumpStream.BufferUsed > 1)
                     {
                         comment = new string(ptr, 0, ((int)minidumpStream.BufferUsed - 2) / 2);
                         return hr;
                     }
                     else
+                    {
                         comment = string.Empty;
+                    }
                     return hr;
                 }
             }
