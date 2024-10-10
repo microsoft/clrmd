3323e090771a6df29bfcc943ef2834a5602d6531
Liu An <liuan@loongson.cn>
2024-10-11 02:46:01 +0800

    [LoongArch64] Fix the LoongArch64Context structure for LoongArch64 (#1289)
    * [LoongArch64] Fix the LoongArch64Context structure for LoongArch64
    
    * Add support for LoongArch64 in the DbgEngDataReader.cs file

--- src/Microsoft.Diagnostics.Runtime/DataReaders/DbgEng/DbgEngDataReader.cs
+++ src/Microsoft.Diagnostics.Runtime/DataReaders/DbgEng/DbgEngDataReader.cs
@@ -128,6 +128,7 @@ namespace Microsoft.Diagnostics.Runtime
             IMAGE_FILE_MACHINE.THUMB or
             IMAGE_FILE_MACHINE.THUMB2 => Architecture.Arm,
             IMAGE_FILE_MACHINE.ARM64 => Architecture.Arm64,
+            IMAGE_FILE_MACHINE.LOONGARCH64 => (Architecture)6 /* Architecture.LoongArch64 */,
             IMAGE_FILE_MACHINE.RISCV64 => (Architecture)9 /* Architecture.RiscV64 */,
             _ => (Architecture)(-1)
         };
